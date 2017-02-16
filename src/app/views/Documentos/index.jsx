f03196b968aa53209486db7ee7ebece61a0b3399
rodrigocipriani <chopsss@gmail.com>
2017-02-16 20:43:14 -0200

    Alterado menu para mobile

--- src/app/views/Documentos/index.jsx
+++ src/app/views/Documentos/index.jsx
@@ -7,6 +7,7 @@ import Painel from 'rc-react-elements/Painel';
 
 const estatuto = '/static/files/minuta-estatuto-associacao-dos-promitentes-compradores-do-residencial-conteporaneo-05-02-2016.pdf';
 const vistoria = '/static/files/Vistoria_20160119_v01.pdf';
+const ata = '/static/files/ata_fundacao-ass_prom_guilherme_almeida-2017-02-14.docx';
 
 @connect(state => ({
     // counter: state.app.get('counter'),
@@ -31,6 +32,12 @@ export default class Documentos extends Component {
 
                 <Painel titulo="Gerais" isOpen={true}>
                     <ul className="collection">
+                        <li className="collection-item">
+                            ATA ASSEMBLEIA DE FUNDAÇÃO - ASSOCIAÇÃO DOS PROMITENTES COMPRADORES DO RESIDENCIAL BOULEVARD CAYMMI (GUILHERME ALMEIDA)
+                            <a href={ata} target="_blank" className="secondary-content">
+                                <i className="fa fa-lg fa-download" aria-hidden="true"></i>
+                            </a>
+                        </li>
                         <li className="collection-item">
                             <small>(Desatualizado) </small>
                             MINUTA ESTATUTO ASSOCIAÇÃO DOS PROMITENTES COMPRADORES DO RESIDENCIAL CONTEMPORÂNEO
