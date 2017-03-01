31c85c1cd54757d8a7af27b7e0f246a57d8850cc
rodrigocipriani <chopsss@gmail.com>
2017-03-01 10:23:46 -0300

    Documentos

--- backend/config/ambiente/producao.js
+++ backend/config/ambiente/producao.js
@@ -1,39 +1,54 @@
 module.exports = {
-  lib: {
-    bcrypt: 'bcript'
-  },
-  mailer: {
-    auth: {
-      user: 'usr.war@gmail.com',
-      pass: 'aerolitos51'
+    lib: {
+        bcrypt: 'bcriptjs'
     },
-    defaultFromAddress: 'nao_responda <nao_responda@guerra.com>'
-  },
-  redis:{
-
-    host: 'redis-15775.c11.us-east-1-3.ec2.cloud.redislabs.com',
-    port: 15775,
-    pass: '',
-    client: '',
-    ttl :  260
-  },
-  postgres: {
-    usuario: 'zwvuhrjpwrsceh',
-      senha: 'q-u9TG1P9Jv2nc8IRH84o3SS_Z',
-      db: 'd259lc4cj3bn94',
-    config: {
-        host: 'ec2-54-243-201-144.compute-1.amazonaws.com',
-      port: 5432,
-      dialect: 'postgres',
-        dialectOptions: {
-            ssl: true
+    mailer: {
+        auth: {
+            user: 'aaa@gmail.com',
+            pass: 'aaa'
         },
-      freezeTableName: true,
-      define: {timestamps: false},
-      pool: {max: 9, min: 0, idle: 10000}
-    }
-  },
-  secretSession: 'domingao faustao',
-  secretCookie: 'pegadinha do malandro',
-  servidor: 'http://pareocarro.herokuapp.com/'
+        defaultFromAddress: 'nao_responda <nao_responda@guerra.com>'
+    },
+    redis: {
+        host: 'localhost',
+        port: 6379,
+        pass: '',
+        client: ''
+        // ttl :  260
+    },
+  /*
+   postgres: {
+   usuario: 'postgres',
+   senha: 'postgres',
+   db: 'condominio',
+   config: {
+   host: '127.0.0.1',
+   port: 5432,
+   dialect: 'postgres',
+   dialectOptions: {
+   ssl: true
+   },
+   // "logging": false,
+   freezeTableName: true,
+   define: {timestamps: false},
+   pool: {max: 9, min: 0, idle: 10000}
+   }
+   },
+   */
+    postgres: {
+        usuario: 'postgres',
+        senha: process.env.POSTGRES_PASSWD,
+        db: 'condominio',
+        config: {
+            host: 'localhost',
+            port: 5432,
+            dialect: 'postgres',
+            freezeTableName: true,
+            define: {timestamps: false},
+            pool: {max: 100, min: 0, idle: 10000}
+        }
+    },
+    secretSession: 'domingao faustao',
+    secretCookie: 'pegadinha do malandro',
+    servidor: 'localhost:3000'
 };
