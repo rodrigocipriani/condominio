46b10893748c6e6910c75d81f8f674d1b8ce620e
rodrigocipriani <chopsss@gmail.com>
2017-03-06 21:50:27 -0300

    Melhorias

--- frontend/src/app/reducers.js
+++ frontend/src/app/reducers.js
@@ -1,4 +1,8 @@
 import {combineReducers} from 'redux';
+
+/**
+ * Importar reducers
+ * */
 import indexReducer from './containers/Index/indexReducer';
 import appReducer from './containers/App/appReducer';
 import documentosReducer from './containers/Documentos/documentosReducer';
@@ -6,6 +10,7 @@ import autenticacaoReducer from './containers/Autenticacao/autenticacaoReducer';
 
 
 export default combineReducers({
+    // incluir reducers
     indexReducer,
     appReducer,
     documentosReducer,
