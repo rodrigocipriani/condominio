e353ff3c9abd7ef8b80f169ca466e0d3910ea694
rodrigocipriani <chopsss@gmail.com>
2017-03-06 21:46:17 -0300

    Melhorias

---
+++ frontend/src/app/config/ambiente/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+    urls: {
+        subApp: "",
+        api: "http://localhost:3000/api",
+        base: "http://localhost:8000"
+    }
+};
--- frontend/src/app/config/index.js
+++ frontend/src/app/config/index.js
@@ -3,7 +3,7 @@
  * */
 let isProduction = process.env.NODE_ENV == 'production';
 
-const development = require('./ambiente/development.json');
-const production = require('./ambiente/production.json');
+const development = require('./ambiente/development');
+const production = require('./ambiente/production');
 
 module.exports = isProduction ? production : development;
